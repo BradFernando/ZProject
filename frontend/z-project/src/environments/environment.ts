2c4b45cc12ab310ec52fb66c73ccccb8b4bcc6fd
bcorro <bcorro@sweadenseguros.com>
2026-04-16 16:36:59 -0500

    Domain

--- frontend/z-project/src/environments/environment.ts
+++ frontend/z-project/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api' // Cambiar por la URL real de producción en el despliegue
+  apiUrl: 'backend-zproject-production.up.railway.app' // Cambiar por la URL real de producción en el despliegue
 };
